24e00137c870c3ead3c182a0b4190f294d98ec9d
TriForMine <quentin.nicolini@hotmail.fr>
2021-02-23 13:26:01 +0100

    Better camera and chunk
    Signed-off-by: TriForMine <quentin.nicolini@hotmail.fr>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bevy::{
     input::{keyboard::KeyCode, system::exit_on_esc_system},
     prelude::*,
 };
-use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
+use bevy_flycam::PlayerPlugin;
 
 use voxel_world_rust::terrain::*;
 
@@ -13,12 +13,10 @@ fn main() {
             title: "Voxel World Rust".to_string(),
             ..Default::default()
         })
-        .insert_resource(ClearColor(Color::BLACK))
-        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(FlyCameraPlugin)
         .add_plugin(TerrainPlugin)
         .add_system(exit_on_esc_system.system())
         .add_startup_system(setup.system())
@@ -27,21 +25,8 @@ fn main() {
 
 fn setup(commands: &mut Commands) {
     commands
-        .spawn(LightBundle {
+        .spawn(PbrBundle {
             transform: Transform::from_translation(Vec3::new(14.0, 18.0, 14.0)),
-            ..Default::default()
-        })
-        .spawn(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(10f32, 10f32, -10f32),
-            ..Default::default()
-        })
-        .with(FlyCamera {
-            pitch: 40.0f32,
-            yaw: -135.0f32,
-            key_up: KeyCode::A,
-            key_down: KeyCode::E,
-            key_left: KeyCode::Q,
-            key_forward: KeyCode::Z,
             ..Default::default()
         });
 }
--- src/terrain.rs
+++ src/terrain.rs
@@ -16,11 +16,16 @@ use core::arch::x86_64::{_mm256_set1_ps, _mm256_storeu_ps};
 use simdnoise::simd;
 use std::collections::{HashMap, HashSet};
 
-type VoxelMap = ChunkHashMap3<Voxel>;
+type VoxelMap = CompressibleChunkMap3<Voxel>;
 type VoxelId = u8;
 
-const SEA_LEVEL: i32 = 64;
-const MAX_CHUNK: usize = 16;
+const CHUNK_SIZE: usize = 32;
+const SEA_LEVEL: i32 = 10;
+const DEFAULT_BUILDER: ChunkMapBuilder3<Voxel> = ChunkMapBuilder3 {
+    chunk_shape: PointN([CHUNK_SIZE as i32; 3]),
+    ambient_value: Voxel(0),
+    default_chunk_metadata: (),
+};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Voxel(VoxelId);
@@ -28,6 +33,12 @@ struct Voxel(VoxelId);
 impl Default for Voxel {
     fn default() -> Self {
         Voxel(0)
+    }
+}
+
+impl Voxel {
+    pub fn voxel_id(&self) -> &u8 {
+        &self.0
     }
 }
 
@@ -55,22 +66,17 @@ impl MergeVoxel for Voxel {
 }
 
 struct VoxelResource {
-    pub map: VoxelMap,
+    pub chunks: VoxelMap,
     pub chunk_size: i32,
     pub materials: Vec<Handle<StandardMaterial>>,
 }
 
 impl Default for VoxelResource {
     fn default() -> Self {
-        let chunk_size = 32;
+        let store = CompressibleChunkStorage::new(Lz4 { level: 10 });
         VoxelResource {
-            chunk_size: chunk_size as i32,
-            map: ChunkMapBuilder3 {
-                chunk_shape: PointN([chunk_size; 3]),
-                ambient_value: Voxel(0),
-                default_chunk_metadata: (),
-            }
-            .build_with_hash_map_storage(),
+            chunk_size: CHUNK_SIZE as i32,
+            chunks: DEFAULT_BUILDER.build_with_write_storage(store),
             materials: Vec::new(),
         }
     }
@@ -89,24 +95,28 @@ impl Default for MeshesResource {
 }
 
 fn generate_chunk(res: &mut ResMut<VoxelResource>, min: Point3i, max: Point3i) {
-    let yscale = 128.0f32;
+    let yscale = 32.0f32;
     for x in min.z()..max.z() {
         for z in min.x()..max.x() {
             unsafe {
                 let max_y = SEA_LEVEL
                     + (if is_x86_feature_detected!("avx2") {
-                        let x_256 = _mm256_set1_ps((x as f32) * 0.02);
-                        let z_256 = _mm256_set1_ps((z as f32) * 0.02);
-                        let s = simdnoise::avx2::simplex_2d(x_256, z_256, 4654545);
-                        let mut r: f32 = 0.0;
-                        _mm256_storeu_ps(&mut r, s);
-                        r
-                    } else {
-                        simdnoise::scalar::simplex_2d((x as f32) * 0.1, (z as f32) * 0.1, 4654545)
-                    } * yscale)
-                        .round() as i32;
+                    let x_256 = _mm256_set1_ps((x as f32) * 0.05);
+                    let z_256 = _mm256_set1_ps((z as f32) * 0.05);
+                    let s = simdnoise::avx2::simplex_2d(x_256, z_256, 4654545);
+                    let mut r: f32 = 0.0;
+                    _mm256_storeu_ps(&mut r, s);
+                    r
+                } else {
+                    simdnoise::scalar::simplex_2d((x as f32) * 0.1, (z as f32) * 0.1, 4654545)
+                } * yscale)
+                    .round() as i32;
                 for y in 0..(max_y + 1) {
-                    *res.map.get_mut(PointN([x, y, z])) = Voxel(1);
+                    if y >= max_y - 3 {
+                        *res.chunks.get_mut(PointN([x, y, z])) = Voxel(1);
+                    } else {
+                        *res.chunks.get_mut(PointN([x, y, z])) = Voxel(2);
+                    }
                 }
             }
         }
@@ -130,31 +140,48 @@ fn generate_voxels(mut voxels: ResMut<VoxelResource>, voxel_meshes: Res<MeshesRe
     }
 }
 
-fn get_mesh(voxel_map: &VoxelMap, pool: &TaskPool) -> Vec<Option<PosNormMesh>> {
+fn get_meshes(voxel_map: &VoxelMap, pool: &TaskPool) -> Vec<Option<HashMap<VoxelId, PosNormMesh>>> {
     pool.scope(|s| {
-        for chunk_key in voxel_map.storage().keys() {
+        let map_ref = &voxel_map;
+        let chunk_keys = map_ref
+            .storage()
+            .chunk_keys()
+            .into_iter()
+            .collect::<Vec<_>>();
+        for chunk_key in chunk_keys {
             s.spawn(async move {
+                let local_cache = LocalChunkCache3::new();
+                let reader = voxel_map.storage().reader(&local_cache);
+                let reader_map = DEFAULT_BUILDER.build_with_read_storage(reader);
+
                 let padded_chunk_extent = padded_greedy_quads_chunk_extent(
-                    &voxel_map.indexer.extent_for_chunk_at_key(*chunk_key),
+                    &reader_map.indexer.extent_for_chunk_at_key(*chunk_key),
                 );
 
                 let mut padded_chunk = Array3::fill(padded_chunk_extent, Voxel(0));
-                copy_extent(&padded_chunk_extent, voxel_map, &mut padded_chunk);
+                copy_extent(&padded_chunk_extent, &reader_map, &mut padded_chunk);
 
                 let mut buffer = GreedyQuadsBuffer::new(padded_chunk_extent);
                 greedy_quads(&padded_chunk, &padded_chunk_extent, &mut buffer);
 
-                let mut mesh = PosNormMesh::default();
+                let mut meshes: HashMap<VoxelId, PosNormMesh> = HashMap::new();
                 for group in buffer.quad_groups.iter() {
                     for quad in group.quads.iter() {
-                        group.face.add_quad_to_pos_norm_mesh(&quad, &mut mesh);
+                        let material = *reader_map.get(quad.minimum).voxel_id();
+                        let mesh = meshes.entry(material).or_insert_with(PosNormMesh::default);
+                        group.face.add_quad_to_pos_norm_mesh(&quad, mesh);
                     }
                 }
 
-                if mesh.is_empty() {
+                let layer_is_empty = meshes.iter().fold(
+                    false,
+                    |acc, (_material, mesh)| if acc { acc } else { mesh.is_empty() },
+                );
+
+                if layer_is_empty {
                     None
                 } else {
-                    Some(mesh)
+                    Some(meshes)
                 }
             })
         }
@@ -172,12 +199,12 @@ fn create_mesh_entity(
 
     let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
     render_mesh.set_attribute(
-        "Vertex_Position",
+        Mesh::ATTRIBUTE_POSITION,
         VertexAttributeValues::Float3(mesh.positions),
     );
-    render_mesh.set_attribute("Vertex_Normal", VertexAttributeValues::Float3(mesh.normals));
+    render_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, VertexAttributeValues::Float3(mesh.normals));
     render_mesh.set_attribute(
-        "Vertex_Uv",
+        Mesh::ATTRIBUTE_UV_0,
         VertexAttributeValues::Float2(vec![[0.0; 2]; num_vertices]),
     );
     render_mesh.set_indices(Some(Indices::U32(mesh.indices)));
@@ -200,21 +227,21 @@ fn generate_meshes(
     pool: Res<ComputeTaskPool>,
 ) {
     let mut to_remove: HashSet<Point3i> = voxel_meshes.generated_map.keys().cloned().collect();
-    for x in 0..8 {
-        for z in 0..8 {
+    for x in 0..4 {
+        for z in 0..4 {
             let p = PointN([x, 0, z]);
             to_remove.remove(&p);
             if voxel_meshes.generated_map.get(&p).is_some() {
                 return;
             }
-            let entity_mesh = get_mesh(&voxels.map, &pool);
+            let entity_mesh = get_meshes(&voxels.chunks, &pool);
             let mut generated_meshes: Vec<Entity> = Vec::new();
-            for mesh in entity_mesh.into_iter() {
-                if let Some(mesh) = mesh {
+            for entity_meshes in entity_mesh.into_iter().flatten() {
+                for (id, mesh) in entity_meshes.into_iter() {
                     generated_meshes.push(create_mesh_entity(
                         mesh,
                         commands,
-                        voxels.materials[1].clone(),
+                        voxels.materials[id as usize].clone(),
                         &mut meshes,
                     ))
                 }
@@ -237,10 +264,16 @@ fn init_materials(
     asset_server: Res<AssetServer>,
 ) {
     let dirt_texture = asset_server.load("blocks/dirt.png");
+    let stone_texture = asset_server.load("blocks/stone.png");
 
     res.materials.push(materials.add(Color::NONE.into()));
     res.materials.push(materials.add(StandardMaterial {
         albedo_texture: Some(dirt_texture),
+        unlit: true,
+        ..Default::default()
+    }));
+    res.materials.push(materials.add(StandardMaterial {
+        albedo_texture: Some(stone_texture),
         unlit: true,
         ..Default::default()
     }));
@@ -253,7 +286,7 @@ impl Plugin for TerrainPlugin {
         app.insert_resource::<VoxelResource>(VoxelResource::default())
             .insert_resource::<MeshesResource>(MeshesResource::default())
             .add_startup_system(init_materials.system())
-            .add_system(generate_voxels.system())
+            .add_startup_system(generate_voxels.system())
             .add_system(generate_meshes.system());
     }
 }
